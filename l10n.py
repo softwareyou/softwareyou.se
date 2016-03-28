ede5dd89c41360e991021d281816ada460d6407f
Daniel Oskarsson <danie@softwareyou.se>
2016-03-28 08:03:42 +0200

    Add translations for CNAME and index.html

--- l10n.py
+++ l10n.py
@@ -3,15 +3,15 @@ import os
 from sys import argv
 
 for l10n_path in glob.glob('**/*.' + argv[1], recursive=True):
-    html_path, _ = os.path.splitext(l10n_path)
-    if os.path.isfile(html_path):
-        with open(html_path, 'r+') as html_file:
-            html_text = html_file.read()
+    path, _ = os.path.splitext(l10n_path)
+    if os.path.isfile(path):
+        with open(path, 'r+') as file:
+            text = file.read()
             with open(l10n_path, 'r') as l10n_file:
                 for line in l10n_file.readlines():
                     values = line.strip().split('=', maxsplit=1)
                     if len(values) == 2:
-                        html_text = html_text.replace("{%s}" % values[0], values[1])
-            html_file.seek(0)
-            html_file.write(html_text)
-            html_file.truncate()
+                        text = text.replace("{%s}" % values[0], values[1])
+            file.seek(0)
+            file.write(text)
+            file.truncate()
