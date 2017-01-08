72b5bf662c1bb54f3d63608f07f41d831cf2f90a
Daniel Oskarsson <daniel.oskarsson@gmail.com>
2017-01-08 22:02:41 +0100

    Use Python 3

--- publish.sh
+++ publish.sh
@@ -9,7 +9,7 @@ git reset --hard origin/gh-pages
 echo "www.softwareyou.$1" > CNAME
 git add CNAME
 git commit CNAME -m "Add CNAME"
-python l10n.py $2
+python3 l10n.py $2
 git add .
 git commit -m "Run l10n"
 git push --force
